9e61ae0d7b17e03f1f98b329b1bb5a28d83f4f68
zong <eruk@qq.com>
2020-04-24 10:26:24 +0800

    try new nltk

--- tieba.py
+++ tieba.py
@@ -20,7 +20,6 @@ def get_content(url):
     comments = []
     # 把需要爬取信息的网页下载到本地
     html = get_html(url)
-
     soup = BeautifulSoup(html, 'lxml')
     # 按照之前的分析，我们找到所有具有‘ j_thread_list clearfix’属性的li标签。返回一个列表类型。
     filename = "soup.txt"
@@ -28,7 +27,7 @@ def get_content(url):
         f.write(soup.encode('utf-8'))
     liTags = soup.find_all("td")
     #liTags = soup.find_all("li")
-    print(liTags)
+    #print(liTags)
     # 通过循环找到每个帖子里的我们需要的信息：
     for li in liTags:
         # 初始化一个字典来存储文章信息
@@ -40,20 +39,28 @@ def get_content(url):
                 'a').text.strip()
             comment['link'] = "http://example.webscraping.com" + \
                 li.find('a')['href']
-            soup_word = BeautifulSoup(comment['link'], features='lxml')
-            print(soup_word)
+            time.sleep(0.3)
+            html_sub = get_html(comment['link'])
+            soup_word = BeautifulSoup(html_sub, features='lxml')
+            #print(soup_word)
             text = soup_word.get_text(strip=True)     
             tokens = [t for t in text.split()]     
             freq = nltk.FreqDist(tokens)     
             for key,val in freq.items(): 
-                comment['value'] = str(val)
-                comment['word'] = str(key)         
-            
+                comment['value'] = val
+                comment['word'] = key
+                print(comment['title'], val, key)
+                with open('hello.txt', 'a+') as f:
+                    f.write('标题：{} \t 数量： {} \t 词：{} \t 链接：{} \n'.format(
+                        comment['title'], comment['value'], comment['word'], comment['link']))
+                        
+            #comments.append(comment)
+            #Out2File(comments)            
             #comment['time'] = li.find(
                 #'span', attrs={'class': 'pull-right is_show_create_time'}).text.strip()
             #comment['replyNum'] = li.find(
                 #'span', attrs={'class': 'threadlist_rep_num center_text'}).text.strip()
-            comments.append(comment)
+            #comments.append(comment)
         except:
             print('ERROR')
 
@@ -63,9 +70,9 @@ def get_content(url):
 def Out2File(dict):
     with open('TTBT.txt', 'a+') as f:
         for comment in dict:
-            f.write('标题： {} \t 链接：{} \t 数量：{} \t 词：{} \n'.format(
+            f.write('标题：{} \t 数量： {} \t 词：{} \t 链接：{} \n'.format(
             #f.write('标题： {} \t 链接：{} \t 发帖人：{} \t 发帖时间：{} \t 回复数量： {} \n'.format(
-                comment['title'], comment['link'], comment['value'], comment['word']))
+                comment['title'], comment['value'], comment['word'], comment['link']))
                 #comment['title'], comment['link'], comment['name'], comment['time'], comment['replyNum']))
 
         print('The current page is crawled')
@@ -80,15 +87,16 @@ def main(base_url, deep):
 
     #循环写入所有的数据
     for url in url_list:
-        content = get_content(url)
-        Out2File(content)
+        get_content(url)
+        #content = get_content(url)
+        #Out2File(content)
     print('data saved')
 
 
 #base_url = 'http://tieba.baidu.com/f?kw=%E7%94%9F%E6%B4%BB%E5%A4%A7%E7%88%86%E7%82%B8&ie=utf-8'
 base_url = 'http://example.webscraping.com/places/default/index'
 # 设置需要爬取的页码数量
-deep = 3
+deep = 40
 
 if __name__ == '__main__':
     main(base_url, deep)
